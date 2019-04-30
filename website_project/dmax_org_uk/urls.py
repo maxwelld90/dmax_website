949ba89dcf7f0af99ff46ec4b9cad95d516e1b3f
David Maxwell <maxwelld90@gmail.com>
2019-04-30 13:40:32 +0100

    Updating base template. Added main header bar

--- website_project/dmax_org_uk/urls.py
+++ website_project/dmax_org_uk/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'dmax_org_uk'
+
 urlpatterns = [
-    path('me/', views.me),
-    path('publications/', views.publications),
-    path('publications/<slug:publication_slug>/', views.publications_entry),
-    path('publications/<slug:publication_slug>/bibtex/', views.publications_bibtex),
-    path('thesis/', views.thesis),
-    path('projects/', views.projects),
-    path('projects/<slug:project_slug>/', views.projects_entry),
-    path('', views.landing),
+    path('me/', views.me, name='me'),
+    path('publications/', views.publications, name='publications'),
+    path('publications/<slug:publication_slug>/', views.publications_entry, name='publications-entry'),
+    path('publications/<slug:publication_slug>/bibtex/', views.publications_bibtex, name='publications-bibtex'),
+    path('thesis/', views.thesis, name='thesis'),
+    path('projects/', views.projects, name='projects'),
+    path('projects/<slug:project_slug>/', views.projects_entry, name='projects-entry'),
+    path('', views.landing, name='landing'),
 ]
