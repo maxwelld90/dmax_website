360b6ffdeee55169d8bff2443dc974b2e3bf569a
David Maxwell <maxwelld90@gmail.com>
2019-04-30 18:53:15 +0100

    Added background images

--- website_project/dmax_org_uk/views.py
+++ website_project/dmax_org_uk/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def landing(request):
-    return render(request, template_name='dmax_org_uk/landing.html')
+    context_dict = {
+        'background': 'shinjuku',
+        'background_identifier': 'Shinjuku, Tokyo, Japan - August 2017'}
+    
+    return render(request, template_name='dmax_org_uk/landing.html', context=context_dict)
 
 def me(request):
     return HttpResponse("me")
 
 def publications(request):
-    return HttpResponse("publications")
+    return render(request, template_name='dmax_org_uk/publications.html')
 
 def publications_entry(request, publication_slug):
     return HttpResponse("publications entry - " + publication_slug)
