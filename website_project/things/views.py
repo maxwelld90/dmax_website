17da80634ac870dfbe5352833ac3e590a313b070
David Maxwell <maxwelld90@gmail.com>
2019-05-20 22:58:42 +0100

    Added logic for thing viewing

--- website_project/things/views.py
+++ website_project/things/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from dmax_org_uk import utils
 from . import models
@@ -10,4 +10,13 @@ def landing(request):
     return render(request, template_name='things/landing.html', context=context_dict)
 
 def entry(request, thing_slug):
+    try:
+        thing = models.Thing.objects.get(slug=thing_slug)
+    except models.Thing.DoesNotExist:
+        return redirect('things:landing')
+    
+    if thing.url_type != models.Thing.THING_URL_INTERNAL:
+        return redirect('things:landing')
+    
+    
     return HttpResponse("thing " + thing_slug)
