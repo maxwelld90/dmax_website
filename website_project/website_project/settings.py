024327c3d9891f3e92872ccf8b51b75b8417b25a
David Maxwell <maxwelld90@gmail.com>
2022-05-12 16:58:33 +0200

    Updated settings

--- website_project/website_project/settings.py
+++ website_project/website_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '=@e+9i6*7z8p^x4m8++gv*lz$hj+-#gk)2te+ynu&(3sw++eye'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['dmax.org.uk', 'www.dmax.org.uk', 'www.dmax.scot', '127.0.0.1']
+ALLOWED_HOSTS = ['dmax.org.uk', 'www.dmax.org.uk', 'dmax.scot', 'www.dmax.scot', '127.0.0.1']
 
 
 # Application definition
